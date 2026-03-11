63d65eab91fae7fc73e0ebb36e22d3b93f7878ab
Giovanni Maria Rava <giovannimaria.rava@studio.unibo.it>
2026-03-11 11:39:07 +0100

    added insert for LAUREA and LAB

--- src/db/insert_data.sql
+++ src/db/insert_data.sql
@@ -37,3 +37,30 @@ insert into EVENTO (titolo, data, oraInizio, durata, numeroLab, numeroAula, loca
 ('GAME AS A LAB', '2026-11-27', '17:00:00', 120, '2.2', null, 'asALAB.jpeg'),
 ('QUANTUM ENGINEERING', '2026-11-19', '17:00:00', 90, '2.3', null, 'Quantum.jpeg'),
 ('FUTURO NELL\'ICT', '2026-11-27', '17:00:00', 120, '2.13', null, 'futuroICT.jpeg');
+
+INSERT INTO LABORATORIO(numeroLab,capienza) VALUES
+('2.2', 98), ('3.1', 42), ('3.3',49), ('4.2', 79), ('CARTA', 2), ('LaMo', 30), ('LaMoV',17), ('LaRAC', 2),
+('LAFO', 16), ('OFF', 30), ('LIB(I)', 50), ('LIB(M)', 40);
+INSERT INTO LAUREA(codiceLaurea, oraInizio, durata, corso, data, numeroAula)
+VALUES 
+
+    (1, '09:00:00', 7, 'Ingegneria e Scienze Informatiche', '2026-07-22', '3.4'),
+    (2, '09:00:00', 7, 'Ingegneria Biomedica', '2026-07-24', '2.1'),
+    (3, '09:00:00', 7, 'Architettura', '2026-07-27', '2.15'),
+    (4, '09:00:00', 7, 'Ingegneria e Scienze Informatiche', '2026-07-29', '2.12'),
+    (5, '09:00:00', 7, 'Ingegneria Biomedica', '2026-09-10', '3.4'),
+    (6, '09:00:00', 7, 'Architettura', '2026-09-15', '2.15'),
+    (7, '09:00:00', 7, 'Ingegneria e Scienze Informatiche', '2026-09-22', '3.4'),
+    (8, '09:00:00', 7, 'Ingegneria Biomedica', '2026-09-28', '2.1'),
+    (9, '09:00:00', 7, 'Architettura', '2026-10-06', '2.15'),
+    (10, '09:00:00', 7, 'Ingegneria e Scienze Informatiche', '2026-10-13', '2.12'),
+    (11, '09:00:00', 7, 'Ingegneria Biomedica', '2026-10-20', '3.4'),
+    (12, '09:00:00', 7, 'Architettura', '2026-10-27', '2.15'),
+    (13, '09:00:00', 7, 'Ingegneria e Scienze Informatiche', '2026-11-05', '3.4'),
+    (14, '09:00:00', 7, 'Ingegneria Biomedica', '2026-11-12', '2.1'),
+    (15, '09:00:00', 7, 'Architettura', '2026-11-19', '2.15'),
+    (16, '09:00:00', 7, 'Ingegneria e Scienze Informatiche', '2026-11-26', '2.12'),
+    (17, '09:00:00', 7, 'Ingegneria Biomedica', '2026-12-03', '3.4'),
+    (18, '09:00:00', 7, 'Architettura', '2026-12-10', '2.15'),
+    (19, '09:00:00', 7, 'Ingegneria e Scienze Informatiche', '2026-12-15', '3.4'),
+    (20, '09:00:00', 7, 'Ingegneria Biomedica', '2026-12-18', '2.1');
