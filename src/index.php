801588596c281430b9d3d0528baa090b40559c51
Riccardo <riccardo.cornacchia@studio.unibo.it>
2026-03-12 23:38:32 +0100

    logo loaded, first page set

--- src/index.php
+++ src/index.php
@@ -31,22 +31,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="it">
 <head>
-    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8"/>
     <title>Login Aule - Accesso</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
 </head>
 <body>
     <header>
-        <div class="logo & title">
+        <div class="logo">
             <a href="index.php">
-                <img src="logo.png" alt="Logo Alma Aule">
+                <img src="upload/uniboLogo.png" alt="Logo Alma Aule">
             </a>
+        </div>
+        <div class="title">
             <h1>Alma Aule</h1>
         </div>
-
-        <nav>
-            <a href="login.php">Login</a>
-        </nav>
+        <div class="login">
+            <nav>
+                <a href="login.php">Login</a>
+            </nav>
+        </div>
     </header>
+    
+    <main>
+
+    </main>
+
     <footer>
         <p>Contatti</p>
     </footer>
