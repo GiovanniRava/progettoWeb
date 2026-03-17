19bd69a6fa5c28686f70d92d6af94d10a8ae9068
Riccardo <riccardo.cornacchia@studio.unibo.it>
2026-03-17 11:45:28 +0100

    sezione prenotazioni e stile

---
+++ src/prenotazioni_studente.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8"/>
+    <title>Prenotazioni-Studente</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="index.php">
+                <img src="upload/uniboLogo.png" alt="Logo Alma Aule">
+            </a>
+        </div>
+        <div class="title">
+            <h1>Alma Aule</h1>
+        </div>
+        <div class="hamburgerMenu" id="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </header>
+    <div class="red-bar">
+        <div class="spacer"></div>
+        <div class="subtitle">
+            <h2>LE MIE PRENOTAZIONI</h2>
+        </div>
+        <div class="add-container">
+            <a href="nuova_prenotazione.php" class="add-box" title="AggiungiPrenotazione">
+                <span class="plus-icon">+</span>
+            </a>
+        </div>
+    </div>
+    
+    <main>
+        <table>
+            <tr>
+                <td>AULA</td>
+                <td>ORARIO</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><input type="submit" name="submit" value="ANNULLA"/></td>
+            </tr>
+        </table>
+    </main>
+
+    <footer>
+        <p>Contatti</p>
+    </footer>
+</body>
