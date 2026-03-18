d1ecedeadffbdfd17efc09994958465e24d1165b
Francesca Gatti <francesca.gatti15@studio.unibo.it>
2026-03-18 12:02:27 +0100

    aggiunto html cerca aula e cerca lab

---
+++ src/cercaAula_studente.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8"/>
+    <title>Cerca aula</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="index.php">
+                <img src="upload/uniboLogo.png" alt="Logo Alma Aule">
+            </a>
+        </div>
+        <div class="title">
+            <h1>Alma Aule</h1>
+        </div>
+        <div class = "hamburgerMenu" id = "hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </header>
+    <div class = "red-bar">
+        <div class = "spacer"></div>
+        <div class = "subtitle">
+            <h2>CERCA AULA</h2>
+        </div>
+        <div class = "spacer"></div>
+    </div>
+
+    <main>
+        <section class="search-bar">
+            <form action="#" method="GET">
+                <div class="input-aule">
+                    <input type="text" id="search" name="search" placeholder="Search...">
+                    <input type="date" id="data-lezione" name="data-lezione">
+                </div>
+                <button type="submit" class = "button-prenota">PRENOTA</button>
+            </form>
+        </section>
+        <section class = "table-aule">
+            <table>
+                <thead>
+                    <tr>
+                        <th>AULA</th>
+                        <th>EVENTO</th>
+                        <th>ORARIO</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2.12</td>
+                        <td>Tecnologie Web</td>
+                        <td>09:00-11-00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    </main>
+</body>
+
+</html> 
