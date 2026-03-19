2ec5763073b87fd90c40fa225aebad913a6275ad
Giovanni Maria Rava <giovannimaria.rava@studio.unibo.it>
2026-03-19 17:49:32 +0100

    created eventi file

---
+++ src/eventi.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8"/>
+    <title>Eventi</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
