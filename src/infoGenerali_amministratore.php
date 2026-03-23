7c6b8d02ae4b238ce51521fb072db8706f7abe2a
Francesca Gatti <francesca.gatti15@studio.unibo.it>
2026-03-23 11:25:52 +0100

    info generali lato amministratore aggiunto

---
+++ src/infoGenerali_amministratore.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8"/>
+    <title>Informazioni Generali - amministrazione</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="index.php">
+                <img src="upload/uniboLogo.png" alt="Logo Alma Aule">
+            </a>
+        </div>
+        <div class="title">
+            <h1>Alma Aule</h1>
+        </div>
+        <div class = "hamburgerMenu" id = "hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </header>
+    <div class = "red-bar">
+        <div class = "spacer"></div>
+        <div class = "subtitle">
+            <h2>INFORMAZIONI GENERALI</h2>
+        </div>
+        <div class = "spacer"></div>
+    </div>
+    
+    <div class="stato-aule">
+        <div class="stato">
+            <h2>46</h2>
+            <p>AULE OCCUPATE</p>
+        </div>
+        <div class="stato">
+            <h2>60</h2>
+            <p>AULE TOTALI</p>
+        </div>
+        <div class="stato">
+            <h2>12</h2>
+            <p>LABORATORI OCCUPATI</p>
+        </div>
+        <div class="stato">
+            <h2>20</h2>
+            <p>LABORATORI TOTALI</p>
+        </div>
+    </div>
+    <div class="eventi">
+        <h2>2</h2>
+        <p>EVENTI IN CORSO</p>
+    </div>
+     <footer>
+        <p>Contatti</p>
+    </footer>
+</body>
+</html>
