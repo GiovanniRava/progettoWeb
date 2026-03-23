26c58ef8eb5dc2b5d8789ff5bd0c029dc1904786
Giovanni Maria Rava <giovannimaria.rava@studio.unibo.it>
2026-03-23 10:04:16 +0100

    created mune for the adimn

---
+++ src/menu_admin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu - Alma Aule</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>        
+        <header class="header-menu">
+            <a href="javascript:history.back()" class="btn-indietro-menu">&gt;</a>
+            <img src="upload/uniboLogo.png" alt="Logo Università" class="logo-menu">
+        </header>
+
+        <div class="red-bar">
+            <div class="spacer"></div>
+            <div class="subtitle">
+                <h2>MENU</h2>
+            </div>
+            <div class="spacer"></div>
+        </div>
+
+        <nav class="menu-navigazione">
+            <ul>
+                <li><a href="prenotazioni_admin.php">PRENOTAZIONI</a></li>
+                <li><a href="richieste_in_corso.php">RICHIESTE IN CORSO</a></li>
+                <li><a href="eventi_admin.php">EVENTI</a></li>
+            </ul>
+        </nav>
+
+        <div class="sezione-logout">
+            <a href="index.php" class="btn-logout">LOGOUT</a>
+        </div>
+        
+
+        <footer>
+            <p>Contatti Per Docenti - Assistenza Didattica tel:0512080302</a></p>
+            <p>Contatti Per Studenti - Help Desk Studenti tel:0512080301 </a></p>
+        </footer>
+
+</body>
+</html>
