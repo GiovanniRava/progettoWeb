f653922c55d54bc91e1707ebc95809a12c8df148
Giovanni Maria Rava <giovannimaria.rava@studio.unibo.it>
2026-03-31 11:43:25 +0200

    add session check for user login in polivalente_base.php

--- src/template/polivalente_base.php
+++ src/template/polivalente_base.php
@@ -1,3 +1,10 @@
+<?php
+if (!isset($_SESSION['utente_loggato'])) {
+    // Se non sei loggato, ti rimando al login
+    header("Location: login.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="it">
 <head>
