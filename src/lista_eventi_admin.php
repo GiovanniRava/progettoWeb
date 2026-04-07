20fea09a11e54e0f6f1f75c95c2de225242ada9e
Giovanni Maria Rava <giovannimaria.rava@studio.unibo.it>
2026-04-07 10:29:42 +0200

    renamed file

--- src/lista_eventi_admin.php
+++ src/lista_eventi_admin.php
@@ -2,5 +2,5 @@
 require_once("bootstrap.php");
 $templateParams["nome"] = "header_pagine.php"; 
 $templateParams["eventi"] = $dbh->get_eventi(); 
-require("template/eventi_admin_base.php");
+require("template/lista_eventi_admin_base.php");
 ?>
