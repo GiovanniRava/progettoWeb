fd9dd192759f774be244013885ca1644f84e121f
Riccardo <riccardo.cornacchia@studio.unibo.it>
2026-04-14 12:44:40 +0200

    fixed css lista prenotazioni. Adedd script directory

---
+++ src/script/prenotazioni_studente_script.js
@@ -0,0 +1,34 @@
+    const classeOrigine = document.currentScript.getAttribute('data-class');
+    
+    document.addEventListener('DOMContentLoaded', function() {
+    const btnBack = document.querySelector('.back-box');
+    const body = document.body;
+    const btnAdd = document.querySelector('.' + classeOrigine);
+
+    if (localStorage.getItem('statoForm') === 'aperto' && window.innerWidth < 768) {
+        body.classList.add('mostra-form');
+    }
+
+    btnAdd.addEventListener('click', function(e) {
+        if (window.innerWidth < 768) {
+            e.preventDefault();
+            body.classList.add('mostra-form');
+            localStorage.setItem('statoForm', 'aperto');
+        }
+    });
+
+    btnBack.addEventListener('click', function(e) {
+        if (window.innerWidth < 768) {
+            e.preventDefault();
+            body.classList.remove('mostra-form');
+            localStorage.removeItem('statoForm');
+            window.history.replaceState({}, '', window.location.pathname);// Questa riga pulisce l'URL (toglie ?inviato=1) al refresh
+        }
+    });
+
+    window.addEventListener('resize', function() {
+        if (window.innerWidth >= 768) {
+            body.classList.remove('mostra-form');
+        }
+    });
+});
