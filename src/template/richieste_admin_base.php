f27f6ac77c12094a28f38f33db64df9670e528c1
Giovanni Maria Rava <giovannimaria.rava@studio.unibo.it>
2026-04-16 15:40:42 +0200

    Refactor admin request logic into separate script file and update template reference

--- src/template/richieste_admin_base.php
+++ src/template/richieste_admin_base.php
@@ -123,65 +123,10 @@ $is_standalone = !isset($is_included_in_main) || $is_included_in_main !== true;
     <form id="form-accetta-richiesta" action="richieste_admin.php" method="POST" style="display: none;">
         <input type="hidden" name="richiesta_da_accettare" id="input-nascosto-accetta" value="">
     </form>
-        
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const bottoniToggle = document.querySelectorAll('.toggle-btn');
-
-            bottoniToggle.forEach(bottone => {
-                bottone.addEventListener('click', function() {
-                    const rigaPrincipale = this.closest('.riga-principale');
-                    
-                    const rigaDettagli1 = rigaPrincipale.nextElementSibling;
-                    const rigaDettagli2 = rigaDettagli1.nextElementSibling;
-                    
-                    const isNascosta = rigaDettagli1.classList.contains('nascosta');
-
-                    if (isNascosta) {
-                        rigaDettagli1.classList.remove('nascosta');
-                        rigaDettagli2.classList.remove('nascosta');
-                        this.innerHTML = '&#708;'; // Freccia in su
-                    } else {
-                        rigaDettagli1.classList.add('nascosta');
-                        rigaDettagli2.classList.add('nascosta');
-                        this.innerHTML = '&#709;'; // Freccia in giù
-                    }
-                });
-            });
-
-            // --- LOGICA RIFIUTA (Con Dialog) ---
-            const dialogRifiuta = document.getElementById('finestra-rifiuta');
-            const inputNascostoRifiuta = document.getElementById('input-nascosto-rifiuta');
-            const btnAnnullaRifiuto = document.getElementById('annulla-rifiuto');
-            const bottoniRifiuta = document.querySelectorAll('.btn-rifiuta');
-
-            bottoniRifiuta.forEach(bottone => {
-                bottone.addEventListener('click', function() {
-                    const idRichiesta = this.getAttribute('data-id');
-                    inputNascostoRifiuta.value = idRichiesta;
-                    dialogRifiuta.showModal();
-                });
-            });
-
-            btnAnnullaRifiuto.addEventListener('click', function() {
-                dialogRifiuta.close();
-                inputNascostoRifiuta.value = ''; // Pulizia
-            });
-
-            // --- LOGICA ACCETTA (Senza Dialog) ---
-            const formAccetta = document.getElementById('form-accetta-richiesta');
-            const inputNascostoAccetta = document.getElementById('input-nascosto-accetta');
-            const bottoniAccetta = document.querySelectorAll('.btn-accetta');
-
-            bottoniAccetta.forEach(bottone => {
-                bottone.addEventListener('click', function() {
-                    const idRichiesta = this.getAttribute('data-id');
-                    inputNascostoAccetta.value = idRichiesta;
-                    formAccetta.submit(); 
-                });
-            });
-        });
-    </script>
+    
+    <script src="script/richieste_admin_script.js"
+    type="text/javascript"></script>
+    
     <?php if ($is_standalone): ?>
         <?php require("footer.php"); ?>
     
