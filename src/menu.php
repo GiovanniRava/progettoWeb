03e4f9bdc21ee2338dc962e67bf868406b1a3534
Giovanni Maria Rava <giovannimaria.rava@studio.unibo.it>
2026-04-19 09:38:31 +0200

    Implement mobile side menu

--- src/menu.php
+++ src/menu.php
@@ -1,10 +1,10 @@
 <?php
 require_once("bootstrap.php");
-$templateParams["nome"] = "header_pagine.php";  
+//$templateParams["nome"] = "header_pagine.php";  
 
 $templateParams["voci_menu"] = [
     ["nome" => "AULE", "url" => "cercaAula_studente.php"],
-    ["nome" => "LABORATORI", "url" => "cercaLab_studente.php"],
+    ["nome" => "LABORATORI", "url" => "cercaLaboratorio_studente.php"],
     ["nome" => "POLIVALENTE", "url" => "polivalente.php"],
     ["nome" => "EVENTI", "url" => "eventi_studente.php"],
     ["nome" => "PRENOTAZIONI", "url" => "prenotazioni_studente.php"]
