22bb6ace0a9b53d625933c37624758c970faffe6
Giovanni Maria Rava <giovannimaria.rava@studio.unibo.it>
2026-04-20 21:59:26 +0200

    Remove unused menu items from student and admin menu files; adjust mobile header menu padding and add hover effect for event cards

--- src/menu.php
+++ src/menu.php
@@ -2,12 +2,6 @@
 require_once("bootstrap.php");
 //$templateParams["nome"] = "header_pagine.php";  
 
-$templateParams["voci_menu"] = [
-    ["nome" => "AULE", "url" => "cercaAula_studente.php"],
-    ["nome" => "LABORATORI", "url" => "cercaLaboratorio_studente.php"],
-    ["nome" => "POLIVALENTE", "url" => "polivalente.php"],
-    ["nome" => "EVENTI", "url" => "eventi_studente.php"],
-    ["nome" => "PRENOTAZIONI", "url" => "prenotazioni_studente.php"]
-];
+
 require("template/menu_base.php");
 ?>
