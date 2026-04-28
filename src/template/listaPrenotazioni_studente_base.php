8305c59ee2cf0a08346f649eaca835adcc415911
Riccardo <riccardo.cornacchia@studio.unibo.it>
2026-04-28 13:20:31 +0200

    messo a posto prenotazioni studente senza triplo main

--- src/template/listaPrenotazioni_studente_base.php
+++ src/template/listaPrenotazioni_studente_base.php
@@ -1,111 +1,106 @@
-<body>
-    <main>
-        <div class="table-container">
-            <table class="table-prenotazioni-studente">
-                <thead>
+<div class="table-container">
+    <table class="table-prenotazioni-studente">
+        <thead>
+            <tr>
+                <th id="numero-aula-lab">AULA / LAB</th>
+                <th id="data-prenotazione">DATA</th>
+                <th id="ora-prenotazione">ORARIO</th>
+                <th id="sezione-annulla"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if(empty($templateParams["prenotazioni"])): ?>
+                <?php if($templateParams["richieste_utente"] == 0): ?>
                     <tr>
-                        <th id="numero-aula-lab">AULA / LAB</th>
-                        <th id="data-prenotazione">DATA</th>
-                        <th id="ora-prenotazione">ORARIO</th>
-                        <th id="sezione-annulla"></th>
+                        <td colspan="4">Nessuna prenotazione in programma.</td>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php if(empty($templateParams["prenotazioni"])): ?>
-                        <?php if($templateParams["richieste_utente"] == 0): ?>
-                            <tr>
-                                <td colspan="4">Nessuna prenotazione in programma.</td>
-                            </tr>
+                <?php else: ?>
+                    <tr>
+                        <td colspan="4">Hai <?= $templateParams["richieste_utente"] ?> richieste pendenti. Attendi che admin le accetti.</td>
+                    </tr>
+                <?php endif; ?>  
+            <?php else:
+                foreach($templateParams["prenotazioni"] as $prenotazione):
+                $dataFormattata = date("d/m/Y", strtotime($prenotazione["data"])); ?>
+                <tr>
+                    <td headers="numero-aula-lab"><?php echo $prenotazione["num"]; ?></td>
+                    <td headers="data-prenotazione"><?php echo $dataFormattata; ?></td>
+                    <td headers="ora-prenotazione"><?php $oraInizio = date("H:i", strtotime($prenotazione["oraInizio"])); echo $oraInizio; ?> - 
+                    <?php $date = new DateTime($prenotazione["oraInizio"]);
+                        $date->modify("+{$prenotazione["durata"]} minutes");
+                        $oraFine = $date->format('H:i'); 
+                        echo $oraFine;
+                    ?></td>
+                    <td headers="sezione-annulla">
+                        <button class="button-annulla-prenotazione" data-id="<?php echo $prenotazione["codicePre"]; ?>">ANNULLA</button>
+                        <!--non uso id perchè superfluo in questo caso-->
+                    </td>
+                </tr>
+                <?php endforeach;
+            endif; ?>
+            <dialog id="finestra-annulla">
+                <h4>ANNULLAMENTO PRENOTAZIONE</h4>
+                <p>Sei sicuro di voler annullare la prenotazione?</p>
+                <form id="form-elimina-prenotazione" action="prenotazioni_studente.php" method="POST">
+                    <input type="hidden" name="nome_da_eliminare" id="input-nascosto-elimina" value="">
+                    <button type="submit" id="conferma-annulla" name="conferma-annulla">SI</button>
+                    <button type="button" id="revoca-annulla" name="revoca-annulla">NO</button>
+                </form>
+            </dialog>
+        </tbody>
+    </table>
+    <h3 class="title-table">RICHIESTE IN CORSO</h3>
+    <table class="table-prenotazioni-studente">
+        <thead>
+            <tr>
+                <th id="numero-aula-lab">AULA / LAB</th>
+                <th id="data-prenotazione">DATA</th>
+                <th id="ora-prenotazione">ORARIO</th>
+                <th id="sezione-annulla"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if(empty($templateParams["richieste_in_corso"])): ?>
+                <?php if($templateParams["richieste_utente"] == 0): ?>
+                    <tr>
+                        <td colspan="4">Nessuna richiesta in programma.</td>
+                    </tr>
+                <?php endif; ?>  
+            <?php else:
+                foreach($templateParams["richieste_in_corso"] as $richiesta):
+                $dataFormattata = date("d/m/Y", strtotime($richiesta["data"])); ?>
+                <tr>
+                    <td headers="numero-aula-lab"><?php echo $richiesta["num"]; ?></td>
+                    <td headers="data-prenotazione"><?php echo $dataFormattata; ?></td>
+                    <td headers="ora-prenotazione"><?php $oraInizio = date("H:i", strtotime($richiesta["oraInizio"])); echo $oraInizio; ?> - 
+                    <?php $date = new DateTime($richiesta["oraInizio"]);
+                        $date->modify("+{$richiesta["durata"]} minutes");
+                        $oraFine = $date->format('H:i'); 
+                        echo $oraFine;
+                    ?></td>
+                    <td headers="sezione-annulla">
+                        <button class="button-elimina-evento" data-id="<?php echo $richiesta["codiceRichiesta"]; ?>">ELIMINA</button>
+                        <?php if($templateParams["azione"] == 1 && $richiesta["codiceRichiesta"] == $templateParams["id"]): ?>
+                            <a href="prenotazioni_studente.php" class="button-modifica-evento">ANNULLA</a>
                         <?php else: ?>
-                            <tr>
-                                <td colspan="4">Hai <?= $templateParams["richieste_utente"] ?> richieste pendenti. Attendi che admin le accetti.</td>
-                            </tr>
-                        <?php endif; ?>  
-                    <?php else:
-                        foreach($templateParams["prenotazioni"] as $prenotazione):
-                        $dataFormattata = date("d/m/Y", strtotime($prenotazione["data"])); ?>
-                        <tr>
-                            <td headers="numero-aula-lab"><?php echo $prenotazione["num"]; ?></td>
-                            <td headers="data-prenotazione"><?php echo $dataFormattata; ?></td>
-                            <td headers="ora-prenotazione"><?php $oraInizio = date("H:i", strtotime($prenotazione["oraInizio"])); echo $oraInizio; ?> - 
-                            <?php $date = new DateTime($prenotazione["oraInizio"]);
-                                $date->modify("+{$prenotazione["durata"]} minutes");
-                                $oraFine = $date->format('H:i'); 
-                                echo $oraFine;
-                            ?></td>
-                            <td headers="sezione-annulla">
-                                <button class="button-annulla-prenotazione" data-id="<?php echo $prenotazione["codicePre"]; ?>">ANNULLA</button>
-                                <!--non uso id perchè superfluo in questo caso-->
-                            </td>
-                        </tr>
-                        <?php endforeach;
-                    endif; ?>
-                    <dialog id="finestra-annulla">
-                        <h4>ANNULLAMENTO PRENOTAZIONE</h4>
-                        <p>Sei sicuro di voler annullare la prenotazione?</p>
-                        <form id="form-elimina-prenotazione" action="prenotazioni_studente.php" method="POST">
-                            <input type="hidden" name="nome_da_eliminare" id="input-nascosto-elimina" value="">
-                            <button type="submit" id="conferma-annulla" name="conferma-annulla">SI</button>
-                            <button type="button" id="revoca-annulla" name="revoca-annulla">NO</button>
-                        </form>
-                    </dialog>
-                </tbody>
-            </table>
-            <h3 class="title-table">RICHIESTE IN CORSO</h3>
-            <table class="table-prenotazioni-studente">
-                <thead>
-                    <tr>
-                        <th id="numero-aula-lab">AULA / LAB</th>
-                        <th id="data-prenotazione">DATA</th>
-                        <th id="ora-prenotazione">ORARIO</th>
-                        <th id="sezione-annulla"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php if(empty($templateParams["richieste_in_corso"])): ?>
-                        <?php if($templateParams["richieste_utente"] == 0): ?>
-                            <tr>
-                                <td colspan="4">Nessuna richiesta in programma.</td>
-                            </tr>
-                        <?php endif; ?>  
-                    <?php else:
-                        foreach($templateParams["richieste_in_corso"] as $richiesta):
-                        $dataFormattata = date("d/m/Y", strtotime($richiesta["data"])); ?>
-                        <tr>
-                            <td headers="numero-aula-lab"><?php echo $richiesta["num"]; ?></td>
-                            <td headers="data-prenotazione"><?php echo $dataFormattata; ?></td>
-                            <td headers="ora-prenotazione"><?php $oraInizio = date("H:i", strtotime($richiesta["oraInizio"])); echo $oraInizio; ?> - 
-                            <?php $date = new DateTime($richiesta["oraInizio"]);
-                                $date->modify("+{$richiesta["durata"]} minutes");
-                                $oraFine = $date->format('H:i'); 
-                                echo $oraFine;
-                            ?></td>
-                            <td headers="sezione-annulla">
-                                <button class="button-elimina-evento" data-id="<?php echo $richiesta["codiceRichiesta"]; ?>">ELIMINA</button>
-                                <?php if($templateParams["azione"] == 1 && $richiesta["codiceRichiesta"] == $templateParams["id"]): ?>
-                                    <a href="prenotazioni_studente.php" class="button-modifica-evento">ANNULLA</a>
-                                <?php else: ?>
-                                    <a href="prenotazioni_studente.php?action=1&id=<?php echo $richiesta["codiceRichiesta"]; ?>" class="button-modifica-evento"> MODIFICA</a>
-                                <?php endif; ?>
-                                </a>
-                            </td>
-                        </tr>
-                        <?php endforeach;
-                    endif; ?>
-                    <dialog id="finestra-elimina">
-                        <h4>ELIMINAZIONE RICHIESTA</h4>
-                        <p>Sei sicuro di voler eliminare la richiesta?</p>
-                        <form id="form-elimina-richiesta" action="prenotazioni_studente.php" method="POST">
-                            <input type="hidden" name="richiesta_da_eliminare" id="input-nascosto-elimina-richiesta" value="">
-                            <button type="submit" id="conferma-elimina" name="conferma-elimina">SI</button>
-                            <button type="button" id="revoca-elimina" name="revoca-elimina">NO</button>
-                        </form>
-                    </dialog>
-                </tbody>
-            </table>
-        </div>
-    </main>
-
-    <script src="script/listaPrenotazioni_studente_script.js"
-    type="text/javascript"></script>
-</body>
+                            <a href="prenotazioni_studente.php?action=1&id=<?php echo $richiesta["codiceRichiesta"]; ?>" class="button-modifica-evento"> MODIFICA</a>
+                        <?php endif; ?>
+                        </a>
+                    </td>
+                </tr>
+                <?php endforeach;
+            endif; ?>
+            <dialog id="finestra-elimina">
+                <h4>ELIMINAZIONE RICHIESTA</h4>
+                <p>Sei sicuro di voler eliminare la richiesta?</p>
+                <form id="form-elimina-richiesta" action="prenotazioni_studente.php" method="POST">
+                    <input type="hidden" name="richiesta_da_eliminare" id="input-nascosto-elimina-richiesta" value="">
+                    <button type="submit" id="conferma-elimina" name="conferma-elimina">SI</button>
+                    <button type="button" id="revoca-elimina" name="revoca-elimina">NO</button>
+                </form>
+            </dialog>
+        </tbody>
+    </table>
+</div>
+<script src="script/listaPrenotazioni_studente_script.js"
+type="text/javascript"></script>
--- src/template/nuova_prenotazione_base.php
+++ src/template/nuova_prenotazione_base.php
@@ -1,107 +1,103 @@
-<body>
-    <main>
-        <div class="form-prenotazione">
-            <form action="prenotazioni_studente.php?action=<?php echo $templateParams["azione"] ?? 0; ?>&id=<?php echo $_GET["id"] ?? ""; ?>" method="POST" class="form-form-prenotazione">
-                <ul>
-                    <li><div class="col-title">
-                        <?php if(isset($templateParams["form"])):
-                            echo $templateParams["form"];
-                        else: ?>
-                            RICHIESTA NUOVA PRENOTAZIONE
-                        <?php endif; ?>
-                    </div></li>
-                    <li>
-                        <div class="col">
-                            <label for="aula-lab">Scegli Aula o Lab</label>
-                            <select class="input-medio" name="aula-lab" id="aula-lab">
-                                <option value="<?php if(isset($templateParams["aula"])): echo $templateParams["aula"]; endif;?>" hidden selected>
-                                    <?php if(isset($templateParams["aula"])): echo $templateParams["aula"]; endif;?>
-                                </option>
-                                <?php foreach($templateParams["aule"] as $aula): ?>
-                                <option value="<?php echo $aula["numeroAula"] ?>"><?php echo $aula["numeroAula"] ?></option>
-                                <?php endforeach; ?>
-                                <?php foreach($templateParams["lab"] as $lab): ?>
-                                <option value="<?php echo $lab["numeroLab"] ?>"><?php echo $lab["numeroLab"] ?></option>
-                                <?php endforeach; ?>
-                            </select>
-                        </div>
-                        <div class="col">
-                            <label for="data">Data</label>
-                            <input type="date" class="input-medio" id="data" name="data"
-                            value="<?php if(isset($templateParams["data"])): echo $templateParams["data"]; endif;?>"/>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="col">
-                            <label for="ora">Orario inizio:</label>
-                            <select class="input-medio" name="oraInizio" id="ora">
-                                <option value="<?php if(isset($templateParams["oraInizio"])): echo $templateParams["oraInizio"]; endif;?>" hidden selected>
-                                    <?php if(isset($templateParams["oraInizio"])): echo $templateParams["oraInizio"]; endif;?>
-                                </option>
-                                <option value="09:00">09:00</option>
-                                <option value="09:30">09:30</option>
-                                <option value="10:00">10:00</option>
-                                <option value="10:30">10:30</option>
-                                <option value="11:00">11:00</option>
-                                <option value="11:30">11:30</option>
-                                <option value="12:00">12:00</option>
-                                <option value="12:30">12:30</option>
-                                <option value="13:00">13:00</option>
-                                <option value="13:30">13:30</option>
-                                <option value="14:00">14:00</option>
-                                <option value="14:30">14:30</option>
-                                <option value="15:00">15:00</option>
-                                <option value="15:30">15:30</option>
-                                <option value="16:00">16:00</option>
-                                <option value="16:30">16:30</option>
-                                <option value="17:00">17:00</option>
-                                <option value="17:30">17:30</option>
-                                <option value="18:00">18:00</option>
-                            </select>
-                        </div>
-                        <div class="col">
-                        <label for="durata">Durata</label>
-                            <select class="input-medio" name="durataPermanenza" id="durata">
-                                <option value="<?php if(isset($templateParams["durata"])): echo $templateParams["durata"]; endif;?>" hidden selected>
-                                    <?php if(isset($templateParams["durata"])): echo $templateParams["durata"]; endif;?>
-                                </option>
-                                <option value="00:30">00:30</option>
-                                <option value="01:00">01:00</option>
-                                <option value="01:30">01:30</option>
-                                <option value="02:00">02:00</option>
-                                <option value="02:30">02:30</option>
-                                <option value="03:00">03:00</option>
-                            </select>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="col">
-                            <label for="nominativo">Nome e Cognome</label>
-                            <input type="text" class="input-pieno" id="nominativo" name="nominativo"
-                            value="<?php if(isset($templateParams["nominativo"])): echo $templateParams["nominativo"]; endif;?>"/>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="col">
-                            <label for="motivazione">Motivazione</label>
-                            <textarea name="motivazionePrenotazione" id="motivazione" rows="7" placeholder="Scrivi..."><?php if(isset($templateParams["motivazione"])): echo htmlspecialchars($templateParams["motivazione"]); endif;?></textarea>
-                        </div>
-                    </li>
-                    <li>
-                        <?php if(isset($templateParams["errore"])): ?>
-                        <p class="error-message"><?php echo $templateParams["errore"]; ?></p>
-                        <?php endif; ?>
-                        <?php if(isset($successo)): ?>
-                        <p class="success-message"><?php echo $successo; ?></p>
-                        <?php endif; ?>
-                    </li>
-                    <li>
-                        <input type="submit" name="submit" class="button-nuova-prenotazione"
-                        value="<?php if(isset($templateParams["button"])): echo $templateParams["button"];
-                        else: ?> INVIA PRENOTAZIONE <?php endif; ?>" />
-                    </li>
-                </ul>
-            </form>
-        </div>
-    </main>
-</body>
+<div class="form-prenotazione">
+    <form action="prenotazioni_studente.php?action=<?php echo $templateParams["azione"] ?? 0; ?>&id=<?php echo $_GET["id"] ?? ""; ?>" method="POST" class="form-form-prenotazione">
+        <ul>
+            <li><div class="col-title">
+                <?php if(isset($templateParams["form"])):
+                    echo $templateParams["form"];
+                else: ?>
+                    RICHIESTA NUOVA PRENOTAZIONE
+                <?php endif; ?>
+            </div></li>
+            <li>
+                <div class="col">
+                    <label for="aula-lab">Scegli Aula o Lab</label>
+                    <select class="input-medio" name="aula-lab" id="aula-lab">
+                        <option value="<?php if(isset($templateParams["aula"])): echo $templateParams["aula"]; endif;?>" hidden selected>
+                            <?php if(isset($templateParams["aula"])): echo $templateParams["aula"]; endif;?>
+                        </option>
+                        <?php foreach($templateParams["aule"] as $aula): ?>
+                        <option value="<?php echo $aula["numeroAula"] ?>"><?php echo $aula["numeroAula"] ?></option>
+                        <?php endforeach; ?>
+                        <?php foreach($templateParams["lab"] as $lab): ?>
+                        <option value="<?php echo $lab["numeroLab"] ?>"><?php echo $lab["numeroLab"] ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <div class="col">
+                    <label for="data">Data</label>
+                    <input type="date" class="input-medio" id="data" name="data"
+                    value="<?php if(isset($templateParams["data"])): echo $templateParams["data"]; endif;?>"/>
+                </div>
+            </li>
+            <li>
+                <div class="col">
+                    <label for="ora">Orario inizio:</label>
+                    <select class="input-medio" name="oraInizio" id="ora">
+                        <option value="<?php if(isset($templateParams["oraInizio"])): echo $templateParams["oraInizio"]; endif;?>" hidden selected>
+                            <?php if(isset($templateParams["oraInizio"])): echo $templateParams["oraInizio"]; endif;?>
+                        </option>
+                        <option value="09:00">09:00</option>
+                        <option value="09:30">09:30</option>
+                        <option value="10:00">10:00</option>
+                        <option value="10:30">10:30</option>
+                        <option value="11:00">11:00</option>
+                        <option value="11:30">11:30</option>
+                        <option value="12:00">12:00</option>
+                        <option value="12:30">12:30</option>
+                        <option value="13:00">13:00</option>
+                        <option value="13:30">13:30</option>
+                        <option value="14:00">14:00</option>
+                        <option value="14:30">14:30</option>
+                        <option value="15:00">15:00</option>
+                        <option value="15:30">15:30</option>
+                        <option value="16:00">16:00</option>
+                        <option value="16:30">16:30</option>
+                        <option value="17:00">17:00</option>
+                        <option value="17:30">17:30</option>
+                        <option value="18:00">18:00</option>
+                    </select>
+                </div>
+                <div class="col">
+                <label for="durata">Durata</label>
+                    <select class="input-medio" name="durataPermanenza" id="durata">
+                        <option value="<?php if(isset($templateParams["durata"])): echo $templateParams["durata"]; endif;?>" hidden selected>
+                            <?php if(isset($templateParams["durata"])): echo $templateParams["durata"]; endif;?>
+                        </option>
+                        <option value="00:30">00:30</option>
+                        <option value="01:00">01:00</option>
+                        <option value="01:30">01:30</option>
+                        <option value="02:00">02:00</option>
+                        <option value="02:30">02:30</option>
+                        <option value="03:00">03:00</option>
+                    </select>
+                </div>
+            </li>
+            <li>
+                <div class="col">
+                    <label for="nominativo">Nome e Cognome</label>
+                    <input type="text" class="input-pieno" id="nominativo" name="nominativo"
+                    value="<?php if(isset($templateParams["nominativo"])): echo $templateParams["nominativo"]; endif;?>"/>
+                </div>
+            </li>
+            <li>
+                <div class="col">
+                    <label for="motivazione">Motivazione</label>
+                    <textarea name="motivazionePrenotazione" id="motivazione" rows="7" placeholder="Scrivi..."><?php if(isset($templateParams["motivazione"])): echo htmlspecialchars($templateParams["motivazione"]); endif;?></textarea>
+                </div>
+            </li>
+            <li>
+                <?php if(isset($templateParams["errore"])): ?>
+                <p class="error-message"><?php echo $templateParams["errore"]; ?></p>
+                <?php endif; ?>
+                <?php if(isset($successo)): ?>
+                <p class="success-message"><?php echo $successo; ?></p>
+                <?php endif; ?>
+            </li>
+            <li>
+                <input type="submit" name="submit" class="button-nuova-prenotazione"
+                value="<?php if(isset($templateParams["button"])): echo $templateParams["button"];
+                else: ?> INVIA PRENOTAZIONE <?php endif; ?>" />
+            </li>
+        </ul>
+    </form>
+</div>
